ca26f05fca4664730e131fe0b7079e4aa3e486a7
Brad Carlson <b2carlso@yahoo.com>
2018-05-04 23:23:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ function driversWithRevenueOver(drivers, query) {
   return drivers.filter(function(driver) {
     return driver.revenue > query})
 }
+
+function driverNamesWithRevenueOver(drivers, query) {
+  driverNames = drivers.filter(function(driver) {return driver.revenue > query})
+  return driverNames.map(function(driver) {return driver.name})
+}

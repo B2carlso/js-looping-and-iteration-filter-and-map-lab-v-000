b35d1cf1b21451928236a915e0e29edf2559f910
Brad Carlson <b2carlso@yahoo.com>
2018-05-04 23:24:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,10 @@ function driverNamesWithRevenueOver(drivers, query) {
   driverNames = drivers.filter(function(driver) {return driver.revenue > query})
   return driverNames.map(function(driver) {return driver.name})
 }
+
+function exactMatch(drivers, query) {
+  return drivers.filter(function(driver) {
+    let key = Object.keys(query)
+    return driver[key] === query[key]
+  })
+}
